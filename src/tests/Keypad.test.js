0be196d980d62bc9051bf52ba39e09be8b782a9b
hthantoo <hthant00chk@gmail.com>
2023-01-18 23:17:22 +0630

    Add tests for calculator components

--- src/tests/Keypad.test.js
+++ src/tests/Keypad.test.js
@@ -1,9 +1,22 @@
 import TestRenderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 import Keypad from '../components/Keypad';
 
-test('Keypad renders correctly', () => {
-  const tree = TestRenderer.create(
-    <Keypad updateCalculation={() => console.log('Calculation updated!')} />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Keypad Component', () => {
+  it('should render correctly', () => {
+    const tree = TestRenderer.create(
+      <Keypad updateCalculation={() => console.log('Calculation updated!')} />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('should contain 19 buttons', async () => {
+    // Arrange
+    render(
+      <Keypad updateCalculation={() => console.log('UpdateCalculation')} />
+    );
+    // Act
+    const buttons = await screen.findAllByRole('button');
+    // Assert
+    expect(buttons).toHaveLength(19);
+  });
 });
